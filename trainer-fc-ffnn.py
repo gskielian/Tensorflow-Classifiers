8f315ee8c956af5dfcf17eb0e4cd9cd9224b2c0a
Gregory S. Kielian <gregory.kielian@gmail.com>
2016-09-17 16:13:32 -0700

    cnn appears torun, but getting bad results

--- trainer-fc-ffnn.py
+++ trainer-fc-ffnn.py
@@ -78,9 +78,6 @@ NUMBER_OF_INPUTS = NUMBER_OF_CAT_IMAGES + NUMBER_OF_DOG_IMAGES
 train_images = np.array(train_images)
 train_images = train_images.reshape(NUMBER_OF_INPUTS,IMAGE_PIXELS)
 
-
-
-
 #dataset class for feeding next batch
 class DataSet(object):
   train_images = []
@@ -121,35 +118,60 @@ data_set = DataSet(train_images, train_labels, FLAGS.batch_size)
 
 test_images, test_labels = data_set.next_batch()
 
+def weight_variable(shape):
+  initial = tf.truncated_normal(shape, stddev=0.1)
+  return tf.Variable(initial)
+
+def bias_variable(shape):
+  initial = tf.constant(0.1, shape=shape)
+  return tf.Variable(initial)
+
+def conv2d(x, W):
+  return tf.nn.conv2d(x, W, strides=[1,1,1,1], padding='SAME')
+
+def max_pool_2x2(x):
+  return tf.nn.max_pool(x, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
+
+
 
 def inference(images, hidden1_units, hidden2_units):
-  # Hidden 1
-  with tf.name_scope('hidden1'):
-    weights = tf.Variable(
-        tf.truncated_normal([IMAGE_PIXELS, hidden1_units],
-                            stddev=1.0 / math.sqrt(float(IMAGE_PIXELS))),
-        name='weights')
-    biases = tf.Variable(tf.zeros([hidden1_units]),
-                         name='biases')
-    hidden1 = tf.nn.relu(tf.matmul(images, weights) + biases)
-  # Hidden 2
-  with tf.name_scope('hidden2'):
-    weights = tf.Variable(
-        tf.truncated_normal([hidden1_units, hidden2_units],
-                            stddev=1.0 / math.sqrt(float(hidden1_units))),
-        name='weights')
-    biases = tf.Variable(tf.zeros([hidden2_units]),
-                         name='biases')
-    hidden2 = tf.nn.relu(tf.matmul(hidden1, weights) + biases)
-  # Linear
+
+  # Convolutional - 1
+  with tf.name_scope('h_conv_1'):
+    W_conv_1 = weight_variable([5,5,1,32])
+    b_conv_1 = bias_variable([32])
+
+    x_image = tf.reshape(images, [-1,28,28, 1])
+    h_conv_1 = tf.nn.relu(conv2d(x_image, W_conv_1) + b_conv_1)
+
+    # Max pool 2x2 - 1
+    h_pool_1 = max_pool_2x2(h_conv_1)
+  # Convolutional - 2
+  with tf.name_scope('h_conv_2'):
+    W_conv_2 = weight_variable([5,5,32,64])
+    b_conv_2 = bias_variable([64])
+
+    h_conv_2 = tf.nn.relu(conv2d(h_pool_1, W_conv_2) + b_conv_2)
+
+    # Max pool 2x2 - 1
+    h_pool_2 = max_pool_2x2(h_conv_2)
+
+  # Densely connected 1
+  with tf.name_scope('densely_connected_1'):
+    W_fc1 = weight_variable([7 * 7 * 64, 1024])
+    b_fc1 = bias_variable([1024])
+
+    h_pool_2_flat = tf.reshape(h_pool_2, [-1, 7*7*64])
+    h_fc1 = tf.nn.relu(tf.matmul(h_pool_2_flat, W_fc1) + b_fc1)
+
+    keep_prob = tf.constant(0.1, dtype=tf.float32)
+    h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+  # Readout Layer
   with tf.name_scope('softmax_linear'):
-    weights = tf.Variable(
-        tf.truncated_normal([hidden2_units, NUM_CLASSES],
-                            stddev=1.0 / math.sqrt(float(hidden2_units))),
-        name='weights')
-    biases = tf.Variable(tf.zeros([NUM_CLASSES]),
-                         name='biases')
-    logits = tf.matmul(hidden2, weights) + biases
+    W_fc2 = weight_variable([1024, 10])
+    b_fc2 = bias_variable([10])
+
+    logits = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
   return logits
 
 def cal_loss(logits, labels):
